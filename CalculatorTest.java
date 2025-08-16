14e44f0d2fcc53c407e97b49ac44217eda358aa0
Sanket Wakchaure <sanketwakchaure45@gmail.com>
2025-08-16 16:07:06 +0530

    subtraction feature is added into calculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,11 +2,18 @@ class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
 }
 
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Result:" + calc.add(10, 20));
+
+        System.out.println("Result:" + calc.sub(10, 20));
+
     }
 }
